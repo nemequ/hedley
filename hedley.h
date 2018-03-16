be4f2fe397e2768aaf7a3f4bb5c70603cf2d4d22
Evan Nemerson <evan@nemerson.com>
2018-03-15 20:09:35 -0700

    Bump version number to 4.

--- hedley.h
+++ hedley.h
@@ -9,11 +9,11 @@
  * For details, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
 
-#if !defined(HEDLEY_VERSION) || (HEDLEY_VERSION < 3)
+#if !defined(HEDLEY_VERSION) || (HEDLEY_VERSION < 4)
 #if defined(HEDLEY_VERSION)
 #  undef HEDLEY_VERSION
 #endif
-#define HEDLEY_VERSION 3
+#define HEDLEY_VERSION 4
 
 #if defined(HEDLEY_VERSION_ENCODE)
 #  undef HEDLEY_VERSION_ENCODE
