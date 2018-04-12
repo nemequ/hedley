a4194fd3ee6a22e447414dd66c044bd01715b10f
Evan Nemerson <evan@nemerson.com>
2018-04-12 16:53:04 -0700

    GCC doesn't support _Static_assert outside of C11 mode until 6.0

--- hedley.h
+++ hedley.h
@@ -985,7 +985,7 @@ HEDLEY_DIAGNOSTIC_POP
 #elif defined(__cplusplus) && __cplusplus >= 201103L
 #  define HEDLEY_STATIC_ASSERT(expr, message) static_assert(expr, message)
 #elif \
-  HEDLEY_GNUC_HAS_FEATURE(c_static_assert,4,6,0) || \
+  HEDLEY_GNUC_HAS_FEATURE(c_static_assert,6,0,0) || \
   HEDLEY_INTEL_VERSION_CHECK(15,0,0)
 #  define HEDLEY_STATIC_ASSERT(expr, message) _Static_assert(expr, message)
 #elif HEDLEY_MSVC_VERSION_CHECK(16,0,0)
