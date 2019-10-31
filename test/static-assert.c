2678c2e271168b5701d82c00c2b8817d194334b8
Evan Nemerson <evan@nemerson.com>
2019-10-31 10:54:57 -0700

    Rework TI compiler support.
    TI uses __TI_COMPILER_VERSION__ for all of its compilers, but there
    is no consistency about what those versions mean across its different
    compilers.  This patch breaks HEDLEY_TI_VERSION into different macros
    for:
    
     * armcl: HEDLEY_TI_ARMCL_VERSION
     * cl430: HEDLEY_TI_CL430_VERSION
     * cl2000: HEDLEY_TI_CL2000_VERSION
     * cl6x: HEDLEY_TI_CL6X_VERSION
     * cl7x: HEDLEY_TI_CL7X_VERSION
     * clpru: HEDLEY_TI_CLPRU_VERSION
    
    And there are associated HEDLEY_TI_*_VERSION_CHECK macros, of course.
    
    The HEDLEY_TI_VERSION macro is preserved as a way to check the MCU
    compilers (ARM, MSP430, and C2000) for newer versions now that those
    three have matching version numbers (for details, see
    https://software-dl.ti.com/ccs/esd/documents/sdto_cgt_lts-and-sts-compiler-releases.html).
    That said, the HEDLEY_TI_{ARMCL,CL430,CL2000}_VERSION must still be
    used for older versions and features not supported by all compilers;
    HEDLEY_TI_VERSION will only be set for versions of armcl/cl430/cl2000
    *after* the new versioning scheme was adopted (≥ 16.0.0).
    
    For testing, I managed to get myhands on of:
    
     * arm_16.9.9.LTS
     * arm_18.12.3.LTS
     * arm_18.1.7.LTS
     * arm_19.6.0.STS
     * c2000_16.9.10.LTS
     * c2000_18.12.3.LTS
     * c2000_18.1.7.LTS
     * c2000_19.6.0.STS
     * c6000_8.0.4
     * c6000_8.1.7
     * c6000_8.2.8
     * c6000_8.3.4
     * c7000_1.2.0.STS
     * msp430_16.9.9.LTS
     * msp430_18.12.3.LTS
     * msp430_18.1.7.LTS
     * msp430_19.6.0.STS
     * pru_2.3.2
    
    And documentation for:
    
     * armcl
       * 16.9.0.LTS
       * 17.6.0.STS
       * 17.9.0.STS
       * 18.1.0.LTS
       * 18.12.0.LTS
       * 19.6.0.STS
       * 2.0.or.earlier
       * 4.7
       * 4.9
       * 5.0
       * 5.1
       * 5.2
     * cl2000
       * 15.12.0.LTS
       * 16.9.0.LTS
       * 17.6.0.STS
       * 17.9.0.STS
       * 18.1.0.LTS
       * 18.12.0.LTS
       * 18.12.3.LTS
       * 18.9.0.STS
       * 19.6.0.STS
       * 4.1.or.older
       * 6.1
       * 6.2.4
       * 6.4
     * cl430
       * 16.9.0.LTS
       * 17.6.0.STS
       * 17.9.0.STS
       * 18.1.0.LTS
       * 18.12.0.LTS
       * 18.12.3.LTS
       * 19.6.0.STS
       * 2.0.1
       * 3.1
       * 3.2
       * 4.0
       * 4.3
       * 4.4
     * cl6x
       * 4.0.or.earlier
       * 6.0
       * 6.1
       * 7.0
       * 7.4
       * 8.1
       * 8.1.x
       * 8.2.x
       * 8.3
     * cl7x
       * 1.2.0
     * clpru
       * 2.1
       * 2.2
       * 2.3
    
    I've tried to implement everything as completely as I could given
    the documentation and software.

--- test/static-assert.c
+++ test/static-assert.c
@@ -4,6 +4,8 @@
 #  pragma diag_suppress=Pe381
 #elif defined(HEDLEY_SUNPRO_VERSION) && !defined(__cplusplus)
 #  pragma error_messages(off,E_EMPTY_DECLARATION)
+#elif defined(HEDLEY_TI_VERSION)
+#  pragma diag_suppress 383
 #endif
 
 HEDLEY_STATIC_ASSERT(sizeof(char) == 1, "char must be one byte");
