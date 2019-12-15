8803445ca726f07ef3f85918131be18c744d535e
Evan Nemerson <evan@nemerson.com>
2019-12-14 17:31:33 -0800

    Check HEDLEY_PREDICT and friends return values
    This is causing an error locally on cl7x, I'm hoping to reproduce it on
    Travis CI.

--- test/likely.c
+++ test/likely.c
@@ -11,12 +11,26 @@
   #include <stddef.h>
 #endif
 
+#if \
+     defined(HEDLEY_TI_CL6X_VERSION) || \
+     defined(HEDLEY_TI_CL7X_VERSION)
+/* __builtin_expect with second argument not equal to 0 or 1 is ignored */
+#  pragma diag_remark 1362
+#endif
+
 static int foo(int x) {
   return HEDLEY_LIKELY(x) ? 1 : 0;
 }
 
 int main(int argc, char* argv[HEDLEY_ARRAY_PARAM(argc)]) {
   assert(argv[argc - 1] != NULL);
+
+  assert(HEDLEY_PREDICT(argc + 1, 2, 0.95) == 2);
+  assert(HEDLEY_UNPREDICTABLE(argc + 1) == 2);
+  assert(HEDLEY_PREDICT_TRUE(argc + 1, 0.95) == 1);
+  assert(HEDLEY_PREDICT_FALSE(argc + 1, 0.95) == 1);
+  assert(HEDLEY_LIKELY(argc + 1) == 1);
+  assert(HEDLEY_UNLIKELY(argc + 1) == 1);
 
   return foo(argc);
 }
